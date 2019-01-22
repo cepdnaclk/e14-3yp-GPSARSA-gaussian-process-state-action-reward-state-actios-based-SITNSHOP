4b6242b1e2c9ae66ff55b70aba91cd25bfe7c7fb
Pankayaraj <pankayaraj1995@gmail.com>
2019-01-22 11:02:07 +0530

    Algorithm Commit
    This is the commit for the backend Algorithm

---
+++ GPSARSA/Kernal/RBF.py
@@ -0,0 +1,27 @@
+# Radial bias kernel
+
+
+import numpy as np
+import tensorflow as tf
+#This was a huge bottelneck see why
+'''
+def RBF_tf(a, b, zigma = 1, variance_weight = 1, datatype = "float64"):
+    v1 = tf.Variable(a, dtype=tf.float32)
+    v2 = tf.Variable(b, dtype=tf.float32)
+
+    euclidian_dist = tf.reduce_sum(tf.square(tf.subtract(v1,v2)))
+    ans = tf.exp(-0.5*euclidian_dist/zigma)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        answer = variance_weight*sess.run(ans)
+
+    return answer
+'''
+def RBF(a, b, zigma=0.01, variance_weight =1):
+    v1 = np.array(a)
+    v2 = np.array(b)
+
+    euclidian_distance = np.sum(np.square(np.subtract(v1,v2)))
+    ans = variance_weight*np.exp(-0.5 *euclidian_distance/zigma)
+
+    return ans
